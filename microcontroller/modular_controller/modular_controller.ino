7ce789c1acf6142aa1be02a11e6e6cd8a5274407
Pau Carre Cardona <pau.carre@gmail.com>
2020-09-06 16:37:59 +0100

    refactored microcontroller code for protocol message

--- microcontroller/modular_controller/modular_controller.ino
+++ microcontroller/modular_controller/modular_controller.ino
@@ -10,14 +10,10 @@
 #define LEFT_MAGNETIC_SENSOR_PIN 14
 #define RIGH_MAGNETIC_SENSOR_PIN 12
 
-RotaryStepper rotary_stepper(false, 27, 26);
-
-RotaryHomer rotary_homer(CENTER_MAGNETIC_SENSOR_PIN, LEFT_MAGNETIC_SENSOR_PIN, RIGH_MAGNETIC_SENSOR_PIN);
-
 struct ConfigurationData {
-  bool dir_high_is_clockwise = rotary_stepper.get_dir_high_is_clockwise();
-  uint8_t direction_pin = rotary_stepper.get_direction_pin();
-  uint8_t step_pin = rotary_stepper.get_step_pin();
+  bool dir_high_is_clockwise = false;
+  uint8_t direction_pin = 27;
+  uint8_t step_pin = 26;
 };
 
 struct ActionsData {
@@ -27,11 +23,11 @@ struct ActionsData {
 
 struct SharedData {
   ActionsData  actions;
-  HomingState homing_state = rotary_homer.homing_state;
+  HomingState homing_state = HOMING_NOT_STARTED;
   ConfigurationData configuration;
 };
 
-SharedData protocol_data;
+SharedData shared_data;
 
 SemaphoreHandle_t mutex;
 
@@ -46,9 +42,7 @@ void do_safely_sharing_data(F &lambda) {
 }
 
 void setup() {
-  rotary_stepper.setup();
-  rotary_homer.setup(rotary_stepper);
-  Serial.begin(9600);// Note that Serial2 is used for communication between microcontroller and host cpu
+  Serial.begin(9600); // NOTE: keep the same in host
   mutex = xSemaphoreCreateMutex();
 
   xTaskCreatePinnedToCore(communication, "communication", 10000, NULL, 1, NULL,  1);
@@ -67,14 +61,26 @@ void write_message(protocol::Message message) {
 
 void control( void * pvParameters ) {
   esp_task_wdt_feed();
-  const TickType_t xDelay = 1 / portTICK_PERIOD_MS;
-  int step = 0;
   SharedData controller_data;
+  RotaryStepper rotary_stepper(false, 27, 26);  
+  rotary_stepper.setup();
+  RotaryHomer rotary_homer(CENTER_MAGNETIC_SENSOR_PIN, LEFT_MAGNETIC_SENSOR_PIN, RIGH_MAGNETIC_SENSOR_PIN);
+  rotary_homer.setup(rotary_stepper);
   for (;;) {
-    auto pull_protocol_configuration = [&] () { controller_data = protocol_data; };
+    auto pull_protocol_configuration = [&] () { controller_data = shared_data; };
     do_safely_sharing_data(pull_protocol_configuration);
     if (controller_data.actions.do_homing) {
       rotary_homer.loop(rotary_stepper);
+      auto update_homing_state = [&] () { shared_data.homing_state = rotary_homer.homing_state; };
+      do_safely_sharing_data(update_homing_state);
+    }
+    if (controller_data.actions.do_configure) {
+      rotary_stepper.configure(
+        controller_data.configuration.dir_high_is_clockwise,
+        controller_data.configuration.direction_pin,
+        controller_data.configuration.step_pin);
+      auto configuration_finished = [&] () { shared_data.actions.do_configure = false; };
+      do_safely_sharing_data(configuration_finished);
     }
   }
 }
@@ -92,7 +98,7 @@ void communication( void * pvParameters ) {
       if (parsing_result.get_is_parsed()) {
         protocol::Message message = parsing_result.get_message();
         if (message.get_message_type() == protocol::HOME_MESSAGE_TYPE) {
-          auto activate_homing = [&] () { protocol_data.actions.do_homing = true; };
+          auto activate_homing = [&] () { shared_data.actions.do_homing = true; };
           do_safely_sharing_data(activate_homing);
           protocol::Message message_return = protocol::Message::make_homing_response_message();
           write_message(message_return);
@@ -101,18 +107,20 @@ void communication( void * pvParameters ) {
           uint8_t direction_pin = message.message[1 + protocol::MESSAGE_DATA_OFFSET_IN_BYTES];
           uint8_t step_pin = message.message[2 + protocol::MESSAGE_DATA_OFFSET_IN_BYTES];
           auto copy_configuration = [&, dir_high_is_clockwise, direction_pin, step_pin] () {
-            protocol_data.configuration.dir_high_is_clockwise = dir_high_is_clockwise;
-            protocol_data.configuration.direction_pin = direction_pin;
-            protocol_data.configuration.step_pin = step_pin;
-            protocol_data.actions.do_configure = true;
+            shared_data.configuration.dir_high_is_clockwise = dir_high_is_clockwise;
+            shared_data.configuration.direction_pin = direction_pin;
+            shared_data.configuration.step_pin = step_pin;
+            shared_data.actions.do_configure = true;
           };
           do_safely_sharing_data(copy_configuration);
           protocol::Message message_return = protocol::Message::make_configure_response_message();
           write_message(message_return);
-        } else if(message.get_message_type() == protocol::HOMING_STATE_MESSAGE_TYPE){          
-          uint8_t homing_state_index = rotary_homer.homing_state;
-          protocol::Message message_return = protocol::Message::make_homing_state_response_message(homing_state_index);
-          write_message(message_return);          
+        } else if(message.get_message_type() == protocol::HOMING_STATE_MESSAGE_TYPE){                   
+          HomingState homing_state = HomingState::HOMING_NOT_STARTED;
+          auto get_homing_state = [&, &homing_state] () { homing_state = shared_data.homing_state; };
+          do_safely_sharing_data(get_homing_state);
+          protocol::Message message_return = protocol::Message::make_homing_state_response_message(homing_state);
+          write_message(message_return);
         }
       }
     }
